57feefbe99a873da2ff7c50c06530d7fe8c57aa7
moudut13 <moudutshuvo40@gmail.com>
2022-05-17 03:59:19 +0600

    react

--- src/Components/Product/Product/AddProduct.js
+++ src/Components/Product/Product/AddProduct.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Select from 'react-select';
+
+
 const AddProduct = () => {
+    const colourOptions = [
+        { value: 'ocean', label: 'Ocean', color: '#00B8D9'},
+        { value: 'blue', label: 'Blue', color: '#0052CC'},
+        { value: 'purple', label: 'Purple', color: '#5243AA' },
+        { value: 'red', label: 'Red', color: '#FF5630'},
+        { value: 'orange', label: 'Orange', color: '#FF8B00' },
+        { value: 'yellow', label: 'Yellow', color: '#FFC400' },
+        { value: 'green', label: 'Green', color: '#36B37E' },
+        { value: 'forest', label: 'Forest', color: '#00875A' },
+        { value: 'slate', label: 'Slate', color: '#253858' },
+        { value: 'silver', label: 'Silver', color: '#666666' },
+    ];
     return(
         <div className="modal fade" id="addProduct">
-            <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-dialog modal-xl" role="document">
                 <form>
                     <div className="modal-content">
                         <div className="modal-header">
@@ -12,17 +27,32 @@ const AddProduct = () => {
                             </button>
                         </div>
                         <div className="modal-body">
-                            <div className="form-group">
-                                <label htmlFor="name">Category Name</label>
-                                <input type="text"
-                                       className="form-control"
-                                       id="name"
-                                       placeholder="Tag Name"
-                                />
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="photo">Photo</label>
-                                <input type="file" className="form-control-file" id="photo"/>
+                            <div className="row">
+                                <div className="col-md-7">
+                                    <div className="form-group">
+                                    <label htmlFor="name">Category Name</label>
+                                    <input type="text"
+                                           className="form-control"
+                                           id="name"
+                                           placeholder="Tag Name"
+                                    />
+                                    </div>
+                                    <div className="form-group">
+                                    <label htmlFor="photo">Photo</label>
+                                    <input type="file" className="form-control-file" id="photo"/>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-5">
+                                    <label htmlFor="name">Tags name</label>
+                                    <Select
+                                        isMulti
+                                        name="colors"
+                                        options={colourOptions}
+                                        className="basic-multi-select"
+                                        classNamePrefix="select"
+                                    />
+                                </div>
                             </div>
                         </div>
                         <div className="modal-footer">
